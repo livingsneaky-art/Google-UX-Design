624ab9e0e354abea4ee4d32ef6178e8b970de298
livingsneaky-art <ryanllamido@gmail.com>
2023-01-22 22:49:29 +0800

    Week 1: Foundation of UX Design

---
+++ Foundations of User Experience (UX) Design/Week_1.js
@@ -0,0 +1,168 @@
+ //Start the program
+ //Welcome to The Google UX Design Certificate
+ /*
+    - Foundations of User Experience (UX) Design
+    - Start the UX Design Process: Empathize, Define, and Ideate
+    - Build Wireframes and Low-Fidelity Prototypes
+    - Conduct UX Research and Test Early Concepts
+    - Create High-Fidelity Designs and Prototypes in Figma
+    - Responsive Web Design in Adobe XD
+    - Design a User Experience for Social Good & Prepare for Jobs
+ */
+//Begin the Google UX Design Certificate
+//Introduction to Course 1: Foundations of User Experience (UX) Design
+/*
+    - When did the study of user experience (UX) begin?
+     1990 Don Norman
+
+    - Foundations of UX degisn
+    - Common terms, tools, and framworks
+    - How to plan and participate in design sprints
+    - Build your professional presence online, including starting your portfolio
+*/
+
+//Welcome to Course 1: Foundations of User Experience (UX) Design
+
+//Your UXD Portfolio Roadmap
+/*
+    Course 1, Foundations of User Experience Design is all about getting 
+    you acquainted with the basics of user experience design. Your 
+    foundational knowledge will help you succeed in starting a portfolio 
+    to present to prospective employers when you start your job search.
+*/
+
+//Program surveys
+//Helpful tips to get started
+/*
+https://www.interaction-design.org/literature
+Interaction Design Foundation: 
+A library of open-source UX design resources, including new articles 
+published every day. There are also local meet-ups (virtual and in-person)
+ that are free to attend and open to everyone. 
+
+https://xd.adobe.com/ideas/?sdid=61PM7WSH&mv=social&mv2=ownsoc-org
+Adobe XD Ideas: A blog curated for entry-level UX designers. You’ll 
+find inspiring examples of great design, valuable career tips, and more.
+
+https://uxdesign.cc/
+UX Collective on Medium: An article platform with stories on user 
+experience, visual design, product design, and more. Articles on 
+Medium are written by a large variety of writers and cover a range 
+of topics.  
+
+https://www.nngroup.com/articles/
+Nielsen Norman Group: Research-based UX guidance, including a ton of 
+helpful articles about the design process, research methods, and user 
+testing, which you'll learn about later in the certificate program.
+*/
+//Intoduct yourself
+//Commit to completing the program
+//Michael - Get stated in UX Design
+
+
+//Get to know user experience design
+//Welcome to week 1
+//The basics of user experience design
+/*
+    - Usable
+    - Equitable
+    - Enjoyable
+    - Useful
+*/
+//Jobs in the field of user experience
+/*
+    - Empathy
+    - Interaction designers
+    - Visual designers
+    - Motion designers
+    - Graphic designers
+    - UX designers
+    - UX researchers
+    - UX writers
+    - Production designers
+    - Assets
+    - UX engineers
+    - UX program managers
+*/
+//User experience careers
+//The product development life cycle
+/*
+    - Product development life cycle
+         Brainstorm
+         Define
+         Design
+         Test
+         Launch
+*/
+//The product development life cycle
+//Design for a good user experience
+//Characteristics of a good user experience
+/*
+    - Usable
+    - Equitable
+    - Enjoyable
+    - Useful
+    https://design.google/library/good-design/
+*/
+//Activity: Indentify good user experience
+//Acitivity Exemplar: Indentify good user experience
+/*
+    MINE: https://docs.google.com/document/d/1DZw1VIXvGER1LBAFUcVQueE8QZ4_w07DYRhZCNpj1d8/edit#
+    Exemplar: https://docs.google.com/document/d/1ZSK2-6S3k2AsXdn53D31Uj0LbKo5FyupYjK3IN3cSjM/edit
+*/
+//Examine user experiences in your life
+
+
+//Explore jobs in user experience
+//Job responsibilities of entry-level UX designers
+/*
+    - Responsibilities of entry-level UX designers
+        Researching
+        Wireframing 
+        Prototyping
+        Creating information architecture
+        Communicating effectively
+*/
+//Dane - A day in the life of an entry-level UX designer
+/*
+    "You'll start by identifying the problems. What are the issues going on
+    within this product? And why are we even looking to improve or introduce
+    this feature?"
+*/
+//Specialist, generalists, and T-shaped designers
+//Erika - Generalist or specialist designer
+//The role of a beginner UX designer
+//Work in a cross-functional team
+//Interact with cross-functional teammates
+
+
+//Understand the types of companies that hire UX designers
+//UX design jobs at different types of companies
+/*
+    - Start-up
+    - Freelancers
+    - Advertising agencies
+    - Design agency
+*/
+//The influence of company size and industry
+
+//Pursue a career in UX design
+//From certificate to career success
+/*
+    - Internships
+    - Apprenticeships
+    - Freelancers
+    - Entry-level jobs
+*/
+//Juan - A UX design career journey
+//Mike - A UX design career journey
+//Goals for exploring UX design
+//Test your knowledge of the roles you can pursue in UX design
+
+//Week 1 review
+//Wrap-up: Introducing user experience design
+//Glossary
+//Weekly challenge 1: The basics of user experience design
+
+//Google UX Design Certificate - Portfolio Project 1
+//https://docs.google.com/presentation/d/1zyqoc1125wnStwfpGwhmJw0PAjtr9-Dt7i0o7a7DYGw/template/preview
