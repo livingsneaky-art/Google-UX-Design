2afe047de20b32fc2c3c43061807c7befb5c6b56
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-01-28 18:20:15 +0800

    Foundations of User Experience (UX) Design Week 2

---
+++ Foundations of User Experience (UX) Design/Week_2.js
@@ -0,0 +1,160 @@
+//Welcome to Week 2
+
+//Thingking like a UX designer
+
+//User-centered design
+/*
+    Puts the user front-and-center 
+    - Understand
+    - Specify
+    - Design
+    - Evaluate
+    
+    //iterations
+*/
+
+//Who are my users?
+/*
+ // To reach as many users as possible, you’ll consider questions such as 
+    these as part of your user research approach:
+    
+    - Do my users have impairments or disabilities to consider–whether temporary, situational, or permanent?
+    - How familiar are my users with technology?
+    - How are my users accessing the product or service?
+    - Where and when are my users accessing the product or service?
+    - Have I considered all my potential users?
+*/
+//Do my users have impairments or disabilities to consider?
+//How familiar are my users with technology?
+//How are my users accessing the product or service?
+/*
+    offline experience
+    https://web.dev/offline-ux-design-guidelines/
+*/
+//Where are my users accessing the product or service?
+//Key takeaway
+/*
+    Your goal as a UX designer is to solve a user problem by creating a design that 
+    makes every user feel like you designed the experience just for them no matter 
+    who they are, where they live, how much they earn, what their abilities are, 
+    or how highly they're educated. Gaining a comprehensive understanding of your 
+    user's context is an important place to start, and you're already on your way.
+*/
+//why user research is important to the field of UX design
+/*
+    https://careerfoundry.com/en/blog/ux-design/the-importance-of-user-research-and-how-to-do-it/
+*/
+
+//Assistive technology
+/*
+    - Color Modification
+    - Voice Control
+    - Screen readers
+    - Alternative text
+*/  
+
+//Elise - The importance of assistive technology
+
+//Akhil - Thinking about users new to technology
+
+//Test your knowlegde on the user
+
+
+//Get to know UX design best practices and trends
+//Design Thinking: A UX design framework 
+/*
+    //framework
+    //Design Thinking framework
+        - Empathize
+        - Define
+        - Ideate
+        - Prototype
+        - Test
+*/
+//Key takeaway
+/*
+    The Design Thinking framework is only one type of framework that UX 
+    designers use to organize their approach to designs, often based on 
+    the product they’re designing and the organization they’re working 
+    for. No matter which frameworks you use in your career, they all 
+    have a few core principles in common:
+
+    - Focus on the user.
+    - Create solutions that address the user’s problems.
+    - Collaborate with teammates across departments.
+    - Validate your designs.
+    - Iterate as needed to design the right user experience.
+*/
+//Design Thinking framework
+/*
+    https://www.youtube.com/watch?v=6lmvCqvmjfE - by Nielsen Norman Group
+    https://www.youtube.com/watch?v=Tvu34s8iMZw - by CareerFoundry
+    https://xd.adobe.com/ideas/guides/ux-design-process-steps/ - by Adobe
+    https://www.interaction-design.org/literature/topics/design-thinking - by The Interaction Design Foundation
+*/
+
+//Universal design, inclusive design, and equity-focused design
+/*
+    - Accessibility
+    - Universal design - One size fits all
+    - Inclusive design - Solve for one, extend to many
+        - Ability
+        - Race
+        - Economic status
+        - Language
+        - Age
+        - Gender
+    - Equity-focused design - Thinking through all the aspects
+        - Equality - providing the same amount of oppurtunity and support
+        - Equity - providing different levels of oppurtunity and support for each
+        person in order to achieve fair outcomes
+*/
+
+//The importance of equity-focused design
+
+//Optional - Additional resource on designing for accessibility
+
+//Get to know platforms
+/*
+    - Platform
+        - Mobile
+        - Desktop
+        - TV
+        - Wearable
+    - Brand Identity
+*/
+
+//Design for different platforms
+/*
+    - Responsive Web Design
+*/
+//Takeaway
+/*
+    - Users behave differently depending on their device
+*/
+
+//Designing cross-platform experiences
+/*
+    - Desktop computers
+    - Laptop computers
+    - Mobile phones
+    - Tablets
+    - Wearables, like smart watches
+    - TVs
+    - Smart displays
+
+    //Screen size
+    //Interaction
+    //Content layout
+    //Functionality
+*/
+
+//Test your knowledge on UX design best practices and trends
+
+
+//Week 2 review
+//Glossary
+
+
+
+
