e0152c73363ad6e9b6457bec90f8b3900b49b2bc
livingsneaky-art <ryanllamido@gmail.com>
2023-01-29 23:46:25 +0800

    Partial Week 3

---
+++ Foundations of User Experience (UX) Design/Week_3.js
@@ -0,0 +1,77 @@
+//Joining design sprints
+//Understand design sprints
+//Welcome to week 3
+
+//Introduction to design sprints
+/*
+    - Design sprint
+       - Design sprint goaal
+    - Website goals value and future vision
+       - Understand 
+       - Ideate
+       - Decide
+       - Prototype
+       - Test
+    - Design sprint benefits
+       - Save time
+       - Create a path to bring a product to market
+       - Prioritize the user
+       - Test product before launch
+    - When to Sprint
+       - Many potential solutions to challenge?
+       - Cross-functional teams needed to weigh in?
+       - Challenge scope wide enough?
+*/
+
+//Five phases of design sprints
+/*
+    - Understand - getting clear picture for the challenge
+    - Ideate - generating ideas
+    - Decide - selecting the best ideas
+    - Prototype - creating a prototype
+    - Test - testing the prototype
+*/
+
+//Benefits of design sprints
+/*
+    - It's all about the user
+    - Value every person in the room
+    - The best ideas rise to the top
+    - Time to focus
+    - Lowers risks
+    - Versatile scheduling
+*/
+
+//Optional - Learn more about design sprints
+/*
+    - Design Sprint Kit https://designsprintkit.withgoogle.com/
+    - Templates for decks and activities https://designsprintkit.withgoogle.com/resources/overview
+*/
+
+//Test your knowledge on design sprints
+
+//Design sprint experience
+
+
+//Plan design sprints
+/*
+    - User research
+    - Call in the experts
+    - Find the right place
+    - Gather supplies
+    - Establish sprint rules
+    - Plan introductions
+    - Post-sprint planning
+*/
+
+//The design sprint brief
+/*
+    - Sprint brief
+    - Sprint leader
+*/
+
+//Test your knowledge on planning a design sprint
+
+
+//Participate in design sprint's
+//An entry-level designer's role in a sprint
