3f3b5fb95eed554ca3ecd4b3bb07a58aa19d7cf5
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-01-31 09:01:14 +0800

    Foundations of User Experience (UX) Design Week 3

--- Foundations of User Experience (UX) Design/Week_3.js
+++ Foundations of User Experience (UX) Design/Week_3.js
@@ -75,3 +75,79 @@
 
 //Participate in design sprint's
 //An entry-level designer's role in a sprint
+/*
+    - Understand
+    - Ideate
+    - Decide
+    - Prototype
+    - Test
+*/
+
+//Organize a design sprint
+
+
+//Jason - All about design sprints
+
+//Self-reflection: Indentify the benefits of design sprints
+
+//Reflect on design sprints
+
+//Design sprint retrospectives
+/*
+    - Retrospective
+*/
+
+//Common retrospective questions
+/*
+    - During a design sprint, the focus is on: 
+    - Understanding the design challenge 
+    - Ideating solutions 
+    - Deciding which solutions to build 
+    - Prototyping a few solutions 
+    - Testing those prototypes
+*/
+//What went well?
+/*
+    - Which tools saved you the most time and effort?
+    - When did you feel the most satisfaction?
+    - What helped you make your best contribution to the team during this sprint?
+*/
+//What can be improved?
+/*
+    - Questions you might ask during this part of the retrospective include:
+    - What went wrong that caught you off guard?
+    - Which problems came up the most often?
+    - When do you think we experienced the biggest challenge as a team?
+    - Then, examine the sprint’s outcome or final product, and ask questions like: 
+    - Did the team overestimate or underestimate the work required to complete the design? 
+    - Did an external factor derail your productivity? 
+    - And most importantly, does the final design actually solve the user problem? 
+
+    - Groupthink
+*/
+//Lesson learned
+/*
+    - Questions you might ask include:
+    - What did you discover during the sprint that you’re still wondering about?
+    - How could the current process be holding the team back from creating better solutions?
+*/
+//Takeaway
+/*
+    Remember: Speak up and share your suggestions for how the next design sprint could be better. 
+    Don’t be afraid to suggest anything you think will improve the project or next sprint. The 
+    only bad suggestion is the one not shared!
+*/
+
+//Self-Reflection: Think througn a retrospective for a previous project
+
+//Group work experience
+
+//Test your knowledge on design sprint retrospectives
+
+//Week 3 review
+//Wrap up week 3
+
+//Glossary
+
+//Week challenge 3: Design sprints
+
