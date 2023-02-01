5a935015698f802de95426d9bad8c61f2b8465bd
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-02-01 22:25:17 +0800

    Partial Week 4

---
+++ Foundations of User Experience (UX) Design/Week_4.js
@@ -0,0 +1,51 @@
+//Understand the role of research in design
+//Integrating research into the design process
+
+//Emily - The power of UX research
+
+//Introduction  to UX research
+/*
+    - UX research
+    - Product development life cycle
+    - Foundational research
+         Answers the questions:
+         - What should we build?
+         - What are the user problems?
+         - How can we solve them?
+         - Am I aware of my own biases, and am I able to filter them as I do research?
+    - Design research
+         Answers the questions:
+         - How should we build it?
+    - Post-launch research
+         Answers the questions:
+         - Did we succeed?
+    - Quality of being a good UX reserach
+        - Empathy
+        - Pragmatism
+        - Collaboration
+*/
+
+//Learn more about UX research
+/*
+    Foundational research
+    Common foundational research methods include: 
+    - Interviews
+    - Surveys
+    - Focus groups
+    - Competitive audit
+    - Field studies
+    - Diary studies
+    Design research
+    Additional research methods that might be used to conduct design research include:
+    - A/B testing
+    - Cafe or guerrilla studies
+    - Card sorting
+    - Intercepts
+    Post-launch research
+*/
+
+//Christie - Design and the importance of feedback
+
+//Test your knowledge on the importance of UX research
+
+//Share your experience of user research
