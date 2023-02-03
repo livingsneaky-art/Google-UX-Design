bd63f873b341cf4b2c3ba04d1d872ebd119ca46c
livingsneaky-art <ryanllamido@gmail.com>
2023-02-03 14:13:00 +0800

    Partial Week 4

--- Foundations of User Experience (UX) Design/Week_4.js
+++ Foundations of User Experience (UX) Design/Week_4.js
@@ -48,4 +48,109 @@
 
 //Test your knowledge on the importance of UX research
 
-//Share your experience of user research
+//Share your experience of user 
+
+
+//Choose the right research method
+/*
+     - Primary research
+     - Secondary research
+     - Quantitatve research - gives you the "what".
+     - Qualitative reseach - gives you the "why".
+
+     - Interviews
+     - Surveys
+     - Usability study
+     - Key preformance indicators (KPIs)
+*/
+
+//Understand benefits and drawbacks of research methods
+/*
+     - Secondary research
+         Benefits:
+           - Saves time and money
+           - Immediately accessible
+           - Backs up primary research
+         Drawbacks:
+           - No first-hand user interaction
+           - No specific user feedback
+     - Interviews
+         Benefits:
+           - Understnad what users  think and why
+           - Ask follow up questions
+         Drawbacks:
+           - Take time and money
+           - Small sample size
+     - Surveys
+         Benefits:
+           - Larger sample sizes
+           - Fast
+           - Inexpensive
+         Drawbacks:
+           - No in-depth feedback 
+     - Usability study
+         Benefits:
+           - Firsthand user interaction
+           - Challenge our assumptions
+           - In-depth feedback
+         Drawbacks:
+           - Only measure how easy a product is to use
+           - Expensive
+           - Difference in lab use versus real life
+*/
+
+//Learn more about research methods
+
+//Choose the best research method
+
+//Craig - My journey to UX
+
+//Test your knowledge on UX research methods
+
+
+//Identify biases in UX research
+//Identify types of bias in UX research
+/*
+     Kinds of biases
+     - Confirmation bias
+     - Flase consensus bias
+     - Primacy bias
+     - Recency bias
+     - Implicit bias
+     - Sunk cost fallacy
+
+     - Bias
+     - Confirmation bias
+     - Overcoming confirmation bias
+     - False consensus bias
+     - Overcoming false consensus bias
+     - Recency bias
+     - Primacy bias
+     - Overcoming recency & primacy bias
+     - Implicit bias
+     - Sunk cost fallacy
+     - Overcoming sunk cost fallacy
+*/
+
+//Learn more about bias in UX research
+/*
+    Preventing bias in data collection
+    - Choose your words carefully
+    - Foster independent thinking
+    - Avoid specific language
+    - Limit the guidance you give users
+    - Consider users' tone and body language
+    - Be careful of your own body language and reactions
+    - Plan your research effectively
+    - Remain open minded
+*/
+
+//Deana - Identify bias in UX research
+
+//Activity: Optional - Implicit bias quiz
+/*
+    https://implicit.harvard.edu/implicit/selectatest.html
+*/
+
+
+
