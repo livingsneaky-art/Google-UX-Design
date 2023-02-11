74578b08d3efe8f3ba3f79a828a1ad3666691b87
livingsneaky-art <ryanllamido@gmail.com>
2023-02-11 23:03:13 +0800

    Partial Week 1

--- Start the UX Design Process Empathize Define and Ideate/Week_1.js
+++ Start the UX Design Process Empathize Define and Ideate/Week_1.js
@@ -40,3 +40,176 @@
     - Keep your own agency
 */
 
+
+//Empathize with users
+/*
+    - Empathy
+
+    UX design is not about solving problems we assume users want solved.
+    It's about solving problems that users actually want solved.
+*/
+
+//Understand empathy in UX design
+/*
+    How to empathize with users
+    - Ask lots of questions
+    - Become more observant
+    - Be an active listener
+    - Request input
+    - Have an open mind
+    - Keep current on UX research
+    
+    Empathy vs sympathy
+    Empathy is sometimes confused with sympathy, but the two terms don't mean the same thing. Empathy means 
+    understanding someone's feelings or thoughts, often by feeling the emotions yourself. Sympathy is the experience
+    of showing concern or compassion without feeling the emotions themselves.
+*/
+
+//Optional: Recruit interview participants
+/*
+    - Screener survey
+    - Determine interview goals
+        - What do you want to learn from the interviews?
+        - Are there certain user problems or pain points that you need to empathize with?
+        - Are there any characteristics of users you want to interview?
+        - Why?
+        - How much information should we have to ensure we get a comprehensive and balanced set of data?
+    - Representative sample
+
+    - Family members
+    - Friends
+    - Current or former colleagues
+    - Managers
+    - Peers in this course
+
+    - Professional networking websites
+    - Online groups based on personal interest
+
+    - Third-party recruiting angency
+    - Websites specifically to connect with research participants
+
+    ***
+    Hello [Participant Name],
+
+    I am currently working on a project for a certificate program where I need to conduct interviews about
+    [explain the project].
+
+    You participation in the research study would be very valuable to help us [summary of project goals]. Details of 
+    the studay are shared below.
+
+    Location: [Address and link to map]
+    Study dates: [Date range]
+    Time: [Time range]
+    Session length: [Estimated length of time for each interview]
+
+    With your permission, we will record each interview to help us decide how to improve our product. Recordings will only be 
+    shared with project team members for the scope of this project. A consent form is attached to this email that you need to sign 
+    and send back.
+
+    If you have any questions, please contact me throught email at [your email address].
+
+    If you are interested in being interviewed, please let me know when you are available between [date range of study] 
+    and the hours of [time range of study].
+
+    Sincerely,
+    [Your Name]
+    ***
+*/
+
+//Determine research goals and questions
+/*
+    Determine research goals
+        - I want to understand the processes and emotions that people experience around the problem my product is trying to solve.
+        - I want to identify common user behaviors and experiences with tasks that my product is trying to address.
+        - I want to understand user needs and frustrations as they relate to the product I'm designing.
+    Write interview questions
+        - Ask open-ended questions
+        - Keep questions short and simple
+        - Ask follow-up questions
+*/
+
+//Activity: Define your research goals and questions
+
+//Activity Exemplar: Define your research goals and questions
+/*
+    Here is a completed exemplar along with an explanation of how the exemplar fulfills the expectations 
+    for the previous activity. This exemplar was completed for a food delivery app called Zia's Pizza. 
+
+    Interview goals: 
+        I want to understand common challenges people face trying to manage a busy schedule and meal planning.
+        I want to identify frustrations people experience during the process of ordering from a restaurant.  
+
+    Interview questions: 
+        Can you describe your current schedule and how you balance your responsibilities with meal planning?
+        How often do you order meals from a restaurant? When you do, what is your motivation for doing so? 
+        What challenges do you face in the ordering process? How does this make you feel?
+        Is there any way in which you feel these challenges could be resolved?
+
+    Target participant characteristics:
+        Ages 18-62
+        Lives in metropolitan or suburban areas
+        People who order food from restaurants at least once a week
+        Include participants of different genders
+        Include participants with disabilities
+*/
+
+//Optional: Find and recruit interview participants
+/*
+    Create a screening survey to find the right participants
+        Screening participants often requires collecting demographics, which are the characteristics 
+        of a group or individual. Demographics that you might ask about in a screener survey include:
+            - Age
+            - Geographic location
+            - Job title or industry
+            - Gender
+    Start recruiting participants
+        - Personal network
+        - Existing user base
+        - Online
+        - Hallway testing 
+        - Third-party recruiting agencies
+*/
+
+//Optional: Prepare for user interviews
+/*
+    Preparing for the interview
+        - Script interview questions
+        - Collect supplies
+        - Research the users
+        - Practice
+*/
+
+//Optional: Interviewing users
+/*
+    Steps for conducting a user interview
+        1. Meet the participant
+           - Build a good rapport
+           - Thank the participant for coming
+           - Review legal details
+           - Gather basic details
+           - Let participant know there are no right or wrong answers
+        2. Conduct the interview
+           - Follow interview etiquette
+           - Ask open-ended questions
+        3. Take notes
+           - Hightlight compelling quotes
+           - Document observations about participants
+           - Consider recording interviews
+        4. Wrap up the interview
+           - Give users a chance to share any final thoughts
+           - Always thank the participant again for their time
+*/
+
+//Optional: Conduct user interviews
+
+//Optional: Conduct user interviews
+
+//Activity: Build empathy with users to inform your research
+/*
+    https://docs.google.com/document/d/1_G6ZuLixniw2vbwo7Qi1LLhpj4g2Ah_KSNDK1wY4LQA/template/preview?resourcekey=0-F9RhhH0XdA6so0Yyj1e4iA
+*/
+
+//Activity Exemplar: Build empathy with users to inform your research
+/*
+    https://docs.google.com/document/d/1OK2DvlpyblFNgQ902h1HNnTG-0oF87pskG2-8yjrZ2c/edit#
+*/
