d3b89151b3b9359b66274f61d972d7aa0012d888
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-03-13 16:42:04 +0800

    Update Week_2.js

--- Start the UX Design Process Empathize Define and Ideate/Week_2.js
+++ Start the UX Design Process Empathize Define and Ideate/Week_2.js
@@ -74,3 +74,75 @@
 
 //Activity Exemplar: Create user journey maps for the CoffeeHouse project
 
+//Consider accessibility
+//Consider accessibility when empathizing
+
+//Consider accessibility during user research
+/*
+    Accessibility
+        Touch
+           - Permanent
+                - One arm
+           - Temporary
+                - Arm injury
+           - Situational   
+                - New parent 
+           Touch: How would you design for users who have use of one arm, either permanently, temporarily, or situationally?
+                - Decide where to place buttons within your design based on several different hand sizes.
+                - Create a feature that allows double taps to avoid accidental icon clicks.
+                - Enable the one-handed keyboard feature and general keyboard compatibility.
+                - Allow button customization for easy access to information that the user finds most important.
+        See
+            - Permanent
+                - Blind
+            - Temporary
+                - Wear glasses
+            - Situational
+                - Distracted driver 
+            See: How would you design for users who have limited vision, either permanently, temporarily, or situationally?
+                - Use a larger font to create a reader-friendly design of the app.
+                - Ensure the app and the images have alternate text that can be read by a screen reader.
+                - Detect whether the user is operating a motor vehicle.
+                - Design the app with high contrast colors.
+                - Don’t rely on text color to explain navigation or next steps. For example, don’t use red text alone as an indicator of a warning. Instead, your design should include explicit instructions.
+    Customize Text
+        Hear
+            - Permanent
+                - Deaf
+            - Temporary
+                - Ear infection
+            - Situational
+                - Bartender
+            Hear: How would you design for users who have limited hearing, either permanently, temporarily, or situationally? 
+                - Don’t rely solely on sounds to provide app updates, like a new message notification. Instead, enable haptics, which are vibrations that engage a user’s sense of touch, and notification lights.
+                - Apply closed captioning to all videos. 
+                - Provide a text messaging system within the app to allow users to communicate through writing.
+        Speak
+            - Permanent
+                - Nonverbal
+            - Temporary
+                - Laryngitis
+            - Situational
+                - Non-native speaker
+            Speak: How would you design for users who cannot speak, either permanently, temporarily, or situationally? 
+                - Provide written intros, descriptions, and instructions for users, in addition to video-based content.
+                - Provide Real-Time Texting during phone calls with users or with app support.
+                - Arrange alternatives for automated systems that rely on speech recognition.
+                - Provide an in-app messaging system that allows the use of emojis and image uploads.
+    Assistive Technologies
+    
+    Putting inclusive design into practice        
+*/
+
+//Understand the curb cut effect
+/*
+    Curb cut
+        - The slope of the sidewalk that creates a ramp with the adjoining street
+
+    Curb cut effect
+        - A phenomenon that describes how products and policies designed for people with disabilities
+            often end up helping everyone
+*/
+
+//Ungraded Plugin: Design accessible experiences
+
