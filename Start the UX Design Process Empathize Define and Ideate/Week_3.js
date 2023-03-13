de905bd86b862fe13c6a9810cca47a9c2b655dc1
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-03-13 22:14:37 +0800

    Done Defining the problem and hypothesis statements

---
+++ Start the UX Design Process Empathize Define and Ideate/Week_3.js
@@ -0,0 +1,92 @@
+//Defining user problems
+//Defining problem and hypothesis statements
+//Welcome to week 3
+/*
+    - Empathize
+    - Define
+    - Ideate
+    - Prototype
+    - Test
+*/
+
+//Create problem statements
+/*
+    Problem statement
+        - A clear description of the user's need that should be addressed
+
+        - Human-centered
+        - Broad enought for creative freedom
+        - Narrow enough to be solved by a design
+
+    *username* is a *user characteristics*
+    who needs *user need*
+    because *insight*.
+
+    Example:
+    Amail is a athelete
+    who needs to sign up for workout classes
+    because the class he wants to participate in fills up fast.
+
+    Problem statement
+        - Establish goals
+        - Understand constraints
+        - Define deliverables
+        - Create benchmarks for success
+*/
+
+//Define problem statements
+/*
+    You learned earlier that there are four categories pain points can fall into: 
+    - Financial, or money-related pain points. 
+    - Product, which are pain points related to quality issues. 
+    - Process, which are pain points related to the user’s journey. 
+    - Support, which are pain points related to getting help from customer service.
+
+    The 5 Ws and H: who, what, when, where, why, and how
+    The most common framework used to create problem statements is the 5 Ws and H framework. 
+    After you define the user’s pain points, you can answer who, what, when, where, why, and 
+        how to solve the user’s problem.
+
+        - Who is experiencing the problem? Knowing your users and their background is key to creating successful solutions for them.
+        - What are the pain points you’re trying to solve? Determining a user’s pain points early allows you to answer the rest of these questions and clarify the context of the pain points.
+        - Where is the user when they’re using the product? A user’s physical context matters to your design.
+        - When does the problem occur? Maybe it’s right after the end of a long and tedious process, or maybe it’s something that happens daily. Knowing when the problem occurs can help you better empathize with the user’s feelings. 
+        - Why is the problem important? Knowing how this problem affects your user’s experience and life will help to clarify the potential consequences.
+        - How are users reaching their goals by using the product? Understanding how users reach their goals allows you to map the user journey that they take through your product.
+
+    The impact of problem statements
+*/
+
+//Optional - Learn more about defining user problems
+
+//Self-Reflection: Problem statements and the 5 Ws framework
+
+//Define hypothesis statements
+/*
+    Hypothesis statement
+        - Our best educated guess on what we think the solution to a design problem might be
+
+    If *action*
+    then *outcome*
+
+    First, you can use the if / then format to come up with a hypothesis statement. Here are examples of if/then hypothesis statements applied to the users of the dog walker app:
+    - If Arnold downloads the dog walker app, then they can utilize the “simplified” mode setting to view only the basic app functions. 
+    - If Tobias signs up for the dog walker app, then they will quickly and easily pick a walker that fits their schedule.
+    Second, you can use the we believe format to come up with a hypothesis statement. Here are examples of we believe hypothesis statements applied to the dog walker app:
+    - We believe that a simplified mode of the dog walker app for Arnold will allow them to hire dog walkers efficiently.
+    - We believe that easy access to available dog walkers for Tobias will increase the amount of walks they choose for their pets. 
+*/
+
+//Practice Activity: Build a problem statement and hypothesis statement for the CoffeeHouse project
+
+//Activity Exemplar: Builad a problem statement and hypothesis statement for the CoffeeHouse project
+
+//Activity: Build a problem statement for your portfolio project
+/*
+    - Add to Case study #8
+*/
+
+//Activity Exemplar: Build a problem statement for your portfolio project
+
+//Share your problem statement
+
