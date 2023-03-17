bf11e152c0d2b0bdaaa0ab738ca2b13670477c41
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-03-17 22:01:36 +0800

    Partial notes for week 4

---
+++ Start the UX Design Process Empathize Define and Ideate/Week_4.js
@@ -0,0 +1,32 @@
+//Ideating design solutions
+//Welcome to week 4
+
+//Understand design ideation
+/*
+    Ideation
+        - The process of generating a broad set of ideas on a given topic, with no attempt to judge
+            or evaluate them
+    
+    Ideation in the real world
+        - Brainstorm out loud
+        - Document all ideas
+        - Focus on quantity
+        - Do not allow evaluation
+        - Gather a diverse team
+        - Question the obvious
+        - Evaluate the ideas
+             - Feasible: Tehnically possible to build
+             - Desirable: Best at solving the user problem
+             - Viable: Financially beneficial for the business
+*/
+
+//Learn more about design ideation
+/*
+    Preparing for ideation
+        - Empathize with your user
+        - Define the problem
+        - Establish a creative environment
+        - Set a time limit
+        - Assemble a diverse team
+        - Think outside the box
+*/
