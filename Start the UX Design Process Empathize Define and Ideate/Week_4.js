b8eebc326138f474eed1f1690e6c45bc77a6d692
livingsneaky-art <ryanllamido@gmail.com>
2023-03-18 23:33:18 +0800

    Update Week_4.js

--- Start the UX Design Process Empathize Define and Ideate/Week_4.js
+++ Start the UX Design Process Empathize Define and Ideate/Week_4.js
@@ -30,3 +30,109 @@
         - Assemble a diverse team
         - Think outside the box
 */
+
+//Explore lots of ideas
+/*
+    Problem
+        - Olivia is a parent who needs a way to ride her bike with her 2-year-old Luca because
+            he can't ride his own bike.
+
+    Why should we come up with a lot of ideas?
+        - List of ideas is narrowed based on constraints
+        - Need to focus on equity
+        - Let users test the ideas
+*/
+
+//Recognize business needs during design ideation
+/*
+    Branding
+        - Two major components of branding are voice and tone.
+
+    Design can help facilitate a converstation between a business and its users.
+*/
+
+//Self-Reflection: Ideation in everyday life
+/*
+    Question 1
+    Describe an example of a time you used the ideation process in the real world. If you can’t think of an example, 
+    think of a project where the ideation process could have been useful. Reflect on the steps you took. How did the 
+    process benefit the projects you worked on? What were the drawbacks?
+
+    - As a software developer, I used the ideation process to brainstorm new features for a web application. I gathered a 
+    team, defined objectives, generated ideas, and evaluated and refined them based on feasibility, impact, and user needs. 
+    We tested the top ideas with users and improved our concepts further. The ideation process helped us prioritize the most 
+    impactful ideas and refine them to create a more user-friendly and effective web application. The ideation process can 
+    be time-consuming and lead to many unfeasible ideas. However, by using a structured approach, we were able to focus on 
+    the most impactful ideas and overcome these potential drawbacks.
+*/
+
+
+//Conduct competitive audits
+//Scope the competition
+/*
+    Competitive audit
+        - An overview of your competitors' strengths and weaknesses
+
+        - Identifying your key competitors
+        - Reviewing the products that your competitors offer
+        - Understanding how your competitors position themselves
+        - Examining what your competition does well and what they could do better
+        - Considering how your competitors talk about themselves
+
+    Direct competitors
+        - Have offerings that are similar to your product and focus on the same audience
+
+    Indirect competitors
+        - Have a similar set of offerings but focus on a different audience, or have a different set of offerings and focus on the same audience
+
+    Benefits to competitive audits
+        - Inform your design process
+        - Solve usability problems
+        - Reveal gaps in the market
+        - Provide reliable evidence
+        - Save time, money, and energy
+*/
+
+//Learn more about competitive audits
+/*
+    Competitive audits can offer many benefits throughout your ideation phase, including:
+        - Giving you an idea of products already in the market and their designs. 
+        - Suggesting ideas to solve early problems that you’re facing with your own designs. 
+        - Revealing the ways that current products in the market are not meeting users’ needs. This is a gap for your product to address!
+        - Demonstrating the expected life cycle of a product in the same market as yours.
+        - Informing all the different iterations your product could take and how those performed for your competitors. 
+*/
+
+//Test your knowledge on competitive audits
+
+//Limits to competitive audits
+/*
+    Limitations of competitive audtis
+        - Stifle creativity
+        - Depend on how well you interpret the findings
+        - Not all designs work in all use cases
+        - Need to be done regularly
+*/
+
+//Steps to conduct a competitive audit
+/*
+    Competitive audit steps
+        - Outline the goals
+        - Create a spreadsheet with a list of your competitors
+        - Call out the specific aspects your want to compare
+        - Research each company
+        - Analyze findings
+        - Summarize findings in a report
+
+    https://docs.google.com/spreadsheets/d/161VBzpqjfF1X9hjp9E5TlbLXOuFrBS4KQsrQke8SZlk/template/preview
+
+    petyou
+    https://docs.google.com/spreadsheets/d/1QnYOWpfROFPs-kDTarTD_Zs2QNSKC1hD6PM7p3zvPSQ/edit?resourcekey=0-mdYEkKny4u2xl53XxgUfCA#gid=2073884517
+    petyou summarize findings report
+    https://docs.google.com/document/d/1aQYL5qb-2x3Bj4-CH215q6Y0dQ4uiluH03a8ITnTg60/edit
+*/
+
+//Practice Activity: Create a competitive audit for the CoffeeHouse project
+
+//Activity Exemplar: Create a competitive audit for the CoffeeHouse project
+
